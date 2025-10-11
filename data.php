432488c1bf6ef79c99b1033a6a41f09629178c28
lukagiorgilomidze <luka-giorgilomidze2003@hotmail.com>
2025-10-11 14:27:35 +0400

    i add change now

--- data.php
+++ data.php
@@ -8,3 +8,17 @@ $navbar_items = [
   ['cv' => 'გადმოწერე რეზიუმე', 'url' => 'https://www.canva.com/design/DAE_KP5gFI0/wOFwptj1FUqBzGk-qUIHOA/edit?utm_content=DAE_KP5gFI0&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton']
   
 ];
+
+$homehero =[
+
+  'title' => 'გამარჯობა! მე ვარ ლუკა-გიორგი ლომიძე',
+  'subtitle' => 'ვებ-დეველოპერი PHP, HTML, CSS და JavaScript-ის სპეციალისტი',
+  'button_text' => 'ნახე ჩემი პროექტები',
+  'button_url' => 'projects.php'
+];
+
+
+$privateinfo = [
+  'image' => './assets/images/500103589_2482070172128541_513000850217612097_n.jpg',
+  'bio' => 'მე ვარ ფასდობილი ვებ-დეველოპერი, რომელიც სპეციალიზებულია PHP-ზე, Angular-ზე და სხვა თანამედროვე ტექნოლოგიებზე. ჩემი მიზანია შევქმნა მომხმარებლისთვის მიმზიდველი და ფუნქციური ვებ-გამოყენებები.'
+];
--- sections/home.php
+++ sections/home.php
@@ -1,8 +1,8 @@
   <!-- მთავარი გვერდი -->
     <section class="hero">
         <div class="text-center">
-            <h1>გამარჯობა! მე ვარ ლუკა გიორგილომიძე</h1>
-            <p>ვებ-დეველოპერი PHP, HTML, CSS და JavaScript-ის სპეციალისტი</p>
-            <a href="projects.php" class="btn btn-custom btn-lg mt-4">ნახე ჩემი პროექტები</a>
+            <h1><?= $homehero['title'] ?></h1>
+            <p><?= $homehero['subtitle'] ?></p>
+            <a href="<?= $homehero['button_url'] ?>" class="btn btn-custom btn-lg mt-4"><?= $homehero['button_text'] ?></a>
         </div>
     </section>
